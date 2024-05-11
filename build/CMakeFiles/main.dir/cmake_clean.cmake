f9253ecf81e20e979e7be47a985eda249be24d15
VoidMiner <VoidMiner@163.com>
2024-05-11 14:03:10 +0800

    CMake Finish

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/algebra.obj"
+  "CMakeFiles/main.dir/src/algebra.obj.d"
+  "CMakeFiles/main.dir/src/main.obj"
+  "CMakeFiles/main.dir/src/main.obj.d"
+  "D:/浙江大学/X-Lab/Homework_1/bin/libmain.dll.a"
+  "D:/浙江大学/X-Lab/Homework_1/bin/main.exe"
+  "D:/浙江大学/X-Lab/Homework_1/bin/main.exe.manifest"
+  "D:/浙江大学/X-Lab/Homework_1/bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
